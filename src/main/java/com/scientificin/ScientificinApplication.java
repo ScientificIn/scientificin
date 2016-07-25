1c2cc4184c51d2845423fed955315f26389b127c
Giulio <giulio.denardi@outlook.com>
2016-07-24 23:12:43 -0300

    FC

---
+++ src/main/java/com/scientificin/ScientificinApplication.java
@@ -0,0 +1,12 @@
+package com.scientificin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScientificinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScientificinApplication.class, args);
+	}
+}
